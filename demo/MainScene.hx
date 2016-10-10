eb50365986470e03e1ea861d7085db832d3d8927
Ben Morris <ben@bendmorris.com>
2016-10-09 21:48:32 -0700

    Add demo project.

---
+++ demo/MainScene.hx
@@ -0,0 +1,55 @@
+import com.haxepunk.Scene;
+
+import haxepunk.ui.Button;
+import haxepunk.ui.Label;
+import haxepunk.ui.PasswordField;
+import haxepunk.ui.TextArea;
+import haxepunk.ui.TextField;
+import haxepunk.ui.ToggleButton;
+import haxepunk.ui.layout.LayoutGroup;
+import haxepunk.ui.MouseManager;
+import haxepunk.ui.RadioButton;
+import haxepunk.ui.RadioButtonGroup;
+
+class MainScene extends Scene
+{
+	override public function begin()
+	{
+		var mouseManager = new MouseManager();
+		mouseManager.type = haxepunk.ui.UIComponent.DEFAULT_TYPE;
+		add(mouseManager);
+
+		var btn = new Button(0, 0, 256, 256, "BIG BUTTON", mouseManager);
+		add(btn);
+
+		var parentLayout = new LayoutGroup(LayoutType.Horizontal);
+		parentLayout.paddingX = parentLayout.paddingY = 16;
+
+		var layout = new LayoutGroup(0.5, LayoutType.Vertical);
+		layout.spacing = 32;
+		layout.add(new Button(0, 0, 128, 32, "Press me", mouseManager));
+		var btn = new Button(0, 0, 128, 32, "Or me", mouseManager);
+		layout.add(btn);
+		layout.add(new Button(0, 0, 128, 32, "Or even me", mouseManager));
+		var grp = new RadioButtonGroup(function() trace("CHANGED"));
+		var btn = new RadioButton(grp, "id1", 0, 0, 24, 24, "Option 1", mouseManager);
+		layout.add(btn);
+		var btn = new RadioButton(grp, "id2", 0, 0, 24, 24, "Option 2", mouseManager);
+		layout.add(btn);
+		var btn = new RadioButton(grp, "id3", 0, 0, 24, 24, "Option 3", mouseManager);
+		layout.add(btn);
+		parentLayout.add(layout);
+
+		var layout = new LayoutGroup(0.5, LayoutType.Vertical);
+		layout.spacing = 8;
+		layout.add(new ToggleButton(10, 40, 100, 25, false, "Toggle me", mouseManager));
+		layout.add(new Label("Read me", 125, 10, 128, 32));
+		layout.add(new TextField("Write something", 125, 40, 200));
+		layout.add(new PasswordField(125, 70, 200));
+		layout.add(new TextArea("You could write a book here!", 330, 10, 300, 228));
+		parentLayout.add(layout);
+
+		add(parentLayout);
+		parentLayout.layoutChildren();
+	}
+}
